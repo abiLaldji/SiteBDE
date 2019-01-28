11486c98c75a612aa3f9fa7f0457c95b09cf31c4
yoann-tillet <33153818+yoann-tillet@users.noreply.github.com>
2019-01-28 08:47:19 +0100

    new file footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -126,6 +126,14 @@ Animation du campus :
 
 </main>
 
+<div class="cookie-stripe">
+  En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de <a href="privacyPolicy"> Cookies</a>  ou autres traceurs.
+  <div class="cookie-buttons">
+  <button values="Accepter" class="cookie-a">Accepter</button>
+  <button values="Refuser" class="cookie-r">Refuser</button>
+</div>
+</div>
+
    <footer> 
 
       <div class="footer-ln">
