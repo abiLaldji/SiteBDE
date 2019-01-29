3fb41fed0c21e0281decac291fb04b94e4beac10
Abigael <abigael@localhost.localdomain>
2019-01-29 16:43:30 +0100

    Final commit

--- public/js/sortPrice.js
+++ public/js/sortPrice.js
@@ -1,17 +1,19 @@
 function sort(array, current_category) {
-    console.log(array);
     
+    // Get the miin and max value to sort
     let min = document.getElementById('input-text-min-aside-section-category').value
     let max = document.getElementById('input-text-max-aside-section-category').value
     if (min == '') min = 0;
     if (max == '') max = 999;
 
+    // Filter all value that in not within min and max
     let p = array.filter(x => x.price >= min && x.price <= max)
 
     let table = document.getElementById('table-category');
 
     table.innerHTML = "";
 
+    // For each value in product array, write a block (row of the table)
     for (let t of p) {
 
         let ih = '<tr class="tr-category"></tr>' +
@@ -26,3 +28,4 @@ function sort(array, current_category) {
         table.innerHTML += ih;
     }
 }
+
